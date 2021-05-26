d49ea81b13dbe18a65835551899353b695500f8f
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-05-26 17:36:01 +0200

    finish 3.13 - fetch from database works

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const dotenv = require('dotenv').config()
+
+const Entry = require('./models/entry')
 
 const app = express()
 
@@ -13,31 +16,10 @@ app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(express.static('build'))
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    phone: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    phone: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    phone: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendick',
-    phone: '39-23-6423122'
-  }
-]
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Entry.find({}).then(entries => {
+    res.json(entries)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
